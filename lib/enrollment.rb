4eb6b02122eddcb911e5dc59011b8f6ffae03637
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-04-20 17:03:05 -0600

    enrollemt class with all tests passing

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -1,24 +1,21 @@
 class Enrollment
 
-  attr_accessor :kindergarten_participation_by_year
-
   def initialize(data)
     @name = data[:name]
     @kindergarten_participation_by_year = data[:kindergarten_participation]
   end
 
-  def truncate_percents
-    require "pry"; binding.pry
-    #access the hash at key
-    #kindergarten_participation
-    #for every value, truncate to 3 decimal points
-  end 
+  def truncate_percents(participation_data)
+    participation_data.map do |date, percent|
+      [date, percent.to_s[0..4].to_f]
+    end.to_h
+  end
+
+  def kindergarten_participation_by_year
+    truncate_percents(@kindergarten_participation_by_year)
+  end
 
   def kindergarten_participation_in_year(year)
-    #want to access the hash at key
-    #kindergarten_participation
-    #and find the value at key year
-
-
+    kindergarten_participation_by_year[year]
   end
 end
