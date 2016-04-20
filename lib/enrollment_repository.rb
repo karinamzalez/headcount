8927e1de1c30c156de8ad84b5221cf798d08893c
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-04-20 17:56:04 -0600

    final stuff from working w/ horace

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class EnrollmentRepository
   attr_reader :enrollments
 
@@ -7,5 +9,19 @@ class EnrollmentRepository
 
   def find_by_name(name)
     enrollments.find { |enrollment| enrollment.name == name }
+  end
+
+  def load_data(data)
+    kindergarten_file = data[:enrollment][:kindergarten]
+
+    raw_csv_data = CSV.open(kindergarten_file, headers: true, header_converters: :symbol).map(&:to_h)
+    raise raw_csv_data.inspect
+    # get the csv
+    # read all the lines
+    # <----------what will i have here
+    # reduce the lines into 1 entry for each district (by name)
+    # <-----------what do i want to have here
+    # create a new Enrollment object to hold this info
+    # add that into the enrollments array
   end
 end
--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -15,7 +15,7 @@ class EnrollmentRepositoryTest < Minitest::Test
 
   def test_it_can_find_a_repository_by_name
     e = Enrollment.new({:name => "ACADEMY 20"})
-    @er.enrollments << e 
+    @er.enrollments << e
     assert_equal "ACADEMY 20", @er.find_by_name("ACADEMY 20").name
   end
 
@@ -23,7 +23,25 @@ class EnrollmentRepositoryTest < Minitest::Test
     assert_equal nil, @er.find_by_name("hello")
   end
 
+  def test_it_can_group_up_a_pile_of_enrollment_data
+    raw_data = [{:location=>"d1", :timeframe=>"2010", :dataformat=>"Percent", :data=>"1"},
+                {:location=>"d1", :timeframe=>"2011", :dataformat=>"Percent", :data=>"1"},
+                {:location=>"d2", :timeframe=>"2012", :dataformat=>"Percent", :data=>"1"},
+                {:location=>"d2", :timeframe=>"2013", :dataformat=>"Percent", :data=>"0.9983"}
+              ]
+    formatted = [{name: "d1", kindergarten_participation: {2010 => "1", 2011 => "1"}},
+                 {name: "d2", kindergarten_participation: {2012 => "1", 2013 => "0.9983"}}]
+    # assert_equal formatted, @er.some_fancy_method(raw_data)
+  end
+
   def test_it_can_load_data_from_a_csv
+    @er.load_data({
+      :enrollment => {
+        :kindergarten => "./test/data/kindergarten.csv"
+      }
+    })
+    assert_equal 4, @er.enrollments.count
+
 
   end
 
