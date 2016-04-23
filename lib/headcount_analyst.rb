9d3e35abb3b495d13f35a7bb35861b87af9ae02a
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-04-23 14:15:22 -0600

    all spec details for iteration 1 working and all tests passing; time to add some more tests and refactor

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -1,6 +1,39 @@
+require 'pry'
 class HeadcountAnalyst
 
-  initialize(dr=)
+  def initialize(dr)
+    @dr = dr
+  end
+
+  def enrollment_data(district_name)
+    district = @dr.find_by_name(district_name)
+    district.enrollment.kindergarten_participation_by_year
+  end
+
+  def participation_percents_for_district(district_name)
+    enrollment_data(district_name).values
+  end
+
+  def average_enrollment(district_name)
+    percents = participation_percents_for_district(district_name)
+    (percents.reduce(:+))/percents.count
+  end
+
+  def kindergarten_participation_rate_variation_trend(district1, district2)
+    district1_percents = participation_percents_for_district(district1)
+    district2_percents = participation_percents_for_district(district2.values[0])
+    district1_years = enrollment_data(district1).keys
+
+    variants = district1_percents.zip(district2_percents).map do |percents|
+      percents.inject(:/).to_s[0..4].to_f
+    end
+
+    district1_years.zip(variants).to_h
+  end
+
+  def kindergarten_participation_rate_variation(district1, district2)
+    raw_variation = average_enrollment(district1)/average_enrollment(district2.values[0])
+    raw_variation.to_s[0..4].to_f
   end
 
 end
---
+++ test/headcount_analyst_test.rb
@@ -0,0 +1,35 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/headcount_analyst'
+require './lib/district_repository'
+
+class HeadcountAnalystTest < Minitest::Test
+
+  def setup
+    @dr = DistrictRepository.new
+    @dr.load_kindergarten_data({
+      :enrollment => {
+        :kindergarten => "./test/data/kindergarten.csv"
+      }
+      })
+    @ha = HeadcountAnalyst.new(@dr)
+  end
+
+  def test_it_can_find_average_enrollment_for_a_district
+    assert_equal 0.337, @ha.average_enrollment("ACADEMY 20")
+  end
+
+  def test_it_can_find_kindergarten_participation_rate_variation
+    assert_equal 1.002, @ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'COLORADO')
+    assert_equal 1.124, @ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'ADAMS COUNTY 14')
+  end
+
+  def test_it_can_find_variation_trends
+    hash = {"2007" => 0.992, "2006" => 1.050, "2005" => 0.960}
+    assert_equal hash, @ha.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO')
+  end
+
+end
