32fcd8eb7685edfa73da8bc71cc36284a9b7f42d
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-04-25 14:59:58 -0600

    started making a statewide test repo

---
+++ test/statewide_test_repository_test.rb
@@ -0,0 +1,8 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/statewide_test_repository'
+
+class StatewideTestRepositoryTest < Minitest::Test
+
+end 
