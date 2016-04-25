750c28eddb3af930f625332645a5dcf88886a466
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-04-25 13:13:24 -0600

    only one more method needed for statewide test class; all tests passing so far

--- lib/statewide_test.rb
+++ lib/statewide_test.rb
@@ -1,24 +1,73 @@
 class StatewideTest
   attr_reader :name,
-              :data
+              :data,
+              :subjects,
+              :grades
 
   def initialize(data)
     @data = data
     @name = data[:name].upcase
+    @subjects = [:math, :reading, :writing]
+    @grades = [3, 8]
   end
+
+  # def grade_to_data_key(grade)
+  #   if grade == 3
+  #     @data[:third_grade_proficiency]
+  #   elsif grade == 8
+  #     @data[:eigth_grade_proficiency]
+  #   end
+  # end
 
   def proficient_by_grade(grade)
     case grade
     when 3
-      @data[:third_grade]
+      @data[:third_grade_proficiency]
     when 8
-      @data[:eigth_grade]
+      @data[:eigth_grade_proficiency]
     else
       raise UnknownDataError
     end
   end
 
+  def proficient_by_race_or_ethnicity(race)
+    case race
+    when :asian
+      @data[:asian_proficiency]
+    when :black
+      @data[:black_proficiency]
+    when :pacific_islander
+      @data[:pacific_islander_proficiency]
+    when :hispanic
+      @data[:hispanic_proficiency]
+    when :native_american
+      @data[:native_american_proficiency]
+    when :two_or_more
+      @data[:two_or_more_proficiency]
+    when :white
+      @data[:white_proficiency]
+    else
+      raise UnknownRaceError
+    end
+  end
+
+  def proficient_for_subject_by_grade_in_year(subject, grade, year)
+      grade_data = proficient_by_grade(grade)
+      if grade_data.keys.include?(year)
+        if subjects.include?(subject)
+          grade_data[year][subject]
+        else
+          raise UnknownDataError
+        end
+      else
+        raise UnknownDataError
+      end
+  end
+
 end
 
 class UnknownDataError < Exception
+end
+
+class UnknownRaceError < Exception
 end
--- test/statewide_test_test.rb
+++ test/statewide_test_test.rb
@@ -9,10 +9,15 @@ class StatewideTestTest < Minitest::Test
     @st = StatewideTest.new(
     {
       :name => "ACADEMY 20",
-      :third_grade =>
+      :third_grade_proficiency =>
       {
        2008 => {:math => 0.857, :reading => 0.866, :writing => 0.671},
        2009 => {:math => 0.824, :reading => 0.862, :writing => 0.706}
+      },
+      :asian_proficiency =>
+      {
+       2011 => {math: 0.816, reading: 0.897, writing: 0.826},
+       2012 => {math: 0.818, reading: 0.893, writing: 0.808}
       }
     })
   end
@@ -27,12 +32,47 @@ class StatewideTestTest < Minitest::Test
   end
 
   def test_it_raises_an_error_if_grade_unknown
-    assert_raises(UnknownDataError) do 
+    assert_raises(UnknownDataError) do
       @st.proficient_by_grade(5)
     end
   end
 
-  def test_it_can_give_proficiency
-    skip
+  def test_it_can_give_proficiency_by_race_or_ethnicity
+    data =
+    {
+     2011 => {math: 0.816, reading: 0.897, writing: 0.826},
+     2012 => {math: 0.818, reading: 0.893, writing: 0.808},
+    }
+    assert_equal data, @st.proficient_by_race_or_ethnicity(:asian)
+  end
+
+  def test_it_raises_an_error_if_race_unknown
+    assert_raises(UnknownRaceError) do
+      @st.proficient_by_race_or_ethnicity(:something)
+    end
+  end
+
+  #deal with edge cases; no data provided so some lines
+  #but probably in statewide test repo class
+
+  def test_it_can_find_proficiency_percent_for_subject_grade_year
+    assert_equal 0.857,
+    @st.proficient_for_subject_by_grade_in_year(:math, 3, 2008)
+  end
+
+  def test_it_raises_error_if_any_parameters_are_invalid
+    assert_raises(UnknownDataError) do
+      @st.proficient_for_subject_by_grade_in_year(:math, 5, 2008)
+    end
+    assert_raises(UnknownDataError) do
+      @st.proficient_for_subject_by_grade_in_year(:astrology, 3, 2008)
+    end
+    assert_raises(UnknownDataError) do
+      @st.proficient_for_subject_by_grade_in_year(:math, 3, 1910)
+    end
+  end
+
+  def method_name
+
   end
 end
