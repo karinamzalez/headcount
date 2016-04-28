05e1bc95ac6924c261e3dd2ed9da7eb606be5a7b
Karina Gonzalez <karinamzalez@gmail.com>
2016-04-28 08:15:33 -0600

    finishing touches test

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -9,7 +9,8 @@ class EnrollmentTest < Minitest::Test
 
   def setup
     @enrollment = Enrollment.new(
-    { :name => "ACADEMY 20",
+    {
+      :name => "ACADEMY 20",
       :kindergarten_participation =>
       {"2010" => "0.3915", "2011" => "0.35356", "2012" => "0.2677"}
     })
