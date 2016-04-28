51db3efcc72c7c3b440a82ddf68b8b6501c87761
Karina Gonzalez <karinamzalez@gmail.com>
2016-04-28 07:48:45 -0600

    some of the free_reduced parser completed

--- lib/parser_poverty_data.rb
+++ lib/parser_poverty_data.rb
@@ -2,7 +2,7 @@ module ParserPovertyData
 
   def format_poverty_hash_per_line(file, name_of_hash)
     lines = get_raw_data(file)
-    l = lines.map do |h|
+    lines.map do |h|
       if h.values.include?("Percent")
         {
           name: h[:location],
@@ -11,4 +11,5 @@ module ParserPovertyData
       end
     end.compact
   end
+
 end
---
+++ test/parser_free_reduced_test.rb
@@ -0,0 +1,69 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/parser_free_reduced'
+require_relative '../lib/simplify_parsers_module'
+require 'csv'
+
+class ParserFreeReducedTest < Minitest::Test
+  include ParserFreeReduced
+  include SimplifyParsers
+
+  def test_it_can_format_each_free_reduced_line_into_hash
+    output =
+      [
+        {
+          :name=>"Colorado",
+          :free_or_reduced_price_lunch=>{2012=>{:number=>"297167"}}
+        },
+        {
+          :name=>"Colorado",
+          :free_or_reduced_price_lunch=>{2012=>{:percent=>0.344}}
+        },
+        {
+          :name=>"Colorado",
+          :free_or_reduced_price_lunch=>{2014=>{:percent=>0.34346}}
+        },
+        {
+          :name=>"Colorado",
+          :free_or_reduced_price_lunch=>{2014=>{:number=>"305342"}}
+        },
+        {
+          :name=>"ACADEMY 20",
+          :free_or_reduced_price_lunch=>{2014=>{:number=>"2156"}}
+        },
+        {
+          :name=>"ACADEMY 20",
+          :free_or_reduced_price_lunch=>{2014=>{:percent=>0.08772}}
+        },
+        {
+          :name=>"ACADEMY 20",
+          :free_or_reduced_price_lunch=>{2012=>{:percent=>0.09027}}
+        },
+        {
+          :name=>"ACADEMY 20",
+          :free_or_reduced_price_lunch=>{2012=>{:number=>"2164"}}
+        },
+        {
+          :name=>"ADAMS COUNTY 14",
+          :free_or_reduced_price_lunch=>{2012=>{:number=>"5486"}}
+        },
+        {
+          :name=>"ADAMS COUNTY 14",
+          :free_or_reduced_price_lunch=>{2012=>{:percent=>0.73147}}
+        },
+        {
+          :name=>"ADAMS COUNTY 14",
+          :free_or_reduced_price_lunch=>{2014=>{:percent=>0.65322}}
+        },
+        {
+          :name=>"ADAMS COUNTY 14", 
+          :free_or_reduced_price_lunch=>{2014=>{:number=>"4954"}}
+        }
+      ]
+    assert_equal output, format_free_reduced_hash_per_line('./test/data/lunch.csv', "free_or_reduced_price_lunch")
+  end
+
+end
