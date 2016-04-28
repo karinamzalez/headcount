db3660ede81c1fd8f67afc5b798478ec4126c1d5
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-04-28 00:47:31 -0600

    made tests for grouping data for median household income and poverty in simplify parser module and created test csv file for lunch data

--- test/simplify_parsers_test.rb
+++ test/simplify_parsers_test.rb
@@ -9,6 +9,7 @@ require_relative '../lib/parser_subject_proficiency'
 require_relative '../lib/parser_median_income'
 require_relative '../lib/simplify_parsers_module'
 require_relative '../test/data/parser_outputs_module'
+require_relative '../lib/parser_poverty_data'
 require 'csv'
 
 class SimplifyParsersTest < Minitest::Test
@@ -18,6 +19,7 @@ class SimplifyParsersTest < Minitest::Test
   include ParserSubjectProficiency
   include ParserMedianIncome
   include ParserOutputs
+  include ParserPovertyData
 
   def test_it_can_get_raw_data_from_csv
     assert_equal grade_raw_data_output,
@@ -74,27 +76,98 @@ class SimplifyParsersTest < Minitest::Test
               2009=>{:math=>0.456, :reading=>0.483, :writing=>0.359}}
             }
         ]
-      assert_equal grade_output, formatted_hashes_per_district('./test/data/3rd_grade.csv', "third_grade_proficiency")
+      assert_equal grade_output,
+      formatted_hashes_per_district('./test/data/3rd_grade.csv', "third_grade_proficiency")
       median_output =
         [
           {
             :name=>"Colorado",
-            :median_household_income=>{[2005, 2009]=>56222, [2006, 2010]=>56456}},
+            :median_household_income=>
+            {[2005, 2009]=>56222, [2006, 2010]=>56456}},
           {
             :name=>"ACADEMY 20",
-            :median_household_income=>{[2005, 2009]=>85060, [2006, 2010]=>85450}},
+            :median_household_income=>
+            {[2005, 2009]=>85060, [2006, 2010]=>85450}},
           {
             :name=>"ADAMS COUNTY 14",
-            :median_household_income=>{[2005, 2009]=>41382, [2006, 2010]=>40740}},
+            :median_household_income=>
+            {[2005, 2009]=>41382, [2006, 2010]=>40740}},
           {
             :name=>"ADAMS-ARAPAHOE 28J",
-            :median_household_income=>{[2005, 2009]=>43893, [2006, 2010]=>44007}},
+            :median_household_income=>
+            {[2005, 2009]=>43893, [2006, 2010]=>44007}},
           {
             :name=>"AGATE 300",
-            :median_household_income=>{[2005, 2009]=>64167, [2006, 2010]=>64145}}]
-      assert_equal median_output, formatted_hashes_per_district('./test/data/median_household_income.csv', "median_household_income")
+            :median_household_income=>
+            {[2005, 2009]=>64167, [2006, 2010]=>64145}}]
+      assert_equal median_output,
+      formatted_hashes_per_district(
+      './test/data/median_household_income.csv', "median_household_income")
     end
 
-    
+    def test_it_can_group_poverty_data
+      data =
+      {
+       "ACADEMY 20"=>
+       [
+         {:name=>"ACADEMY 20",
+          :children_in_poverty=>{2007=>0.039}},
+         {:name=>"ACADEMY 20",
+          :children_in_poverty=>{2008=>0.044}}],
+       "ADAMS COUNTY 14"=>
+       [
+         {:name=>"ADAMS COUNTY 14",
+          :children_in_poverty=>{2007=>0.247}},
+         {:name=>"ADAMS COUNTY 14",
+          :children_in_poverty=>{2008=>0.225}}],
+       "ADAMS-ARAPAHOE 28J"=>
+       [
+         {:name=>"ADAMS-ARAPAHOE 28J",
+          :children_in_poverty=>{2007=>0.238}},
+         {:name=>"ADAMS-ARAPAHOE 28J",
+          :children_in_poverty=>{2008=>0.185}}]}
+      assert_equal data,
+      group_by_district_name('./test/data/poverty.csv', "children_in_poverty")
+    end
+
+    def test_it_can_group_median_household_income_data
+      data =
+      {
+        "Colorado"=>
+        [
+          {:name=>"Colorado",
+             :median_household_income=>{[2005, 2009]=>56222}},
+           {:name=>"Colorado",
+              :median_household_income=>{[2006, 2010]=>56456}}],
+       "ACADEMY 20"=>
+       [
+         {:name=>"ACADEMY 20",
+            :median_household_income=>{[2005, 2009]=>85060}},
+          {:name=>"ACADEMY 20",
+             :median_household_income=>{[2006, 2010]=>85450}}],
+        "ADAMS COUNTY 14"=>
+        [
+          {:name=>"ADAMS COUNTY 14",
+             :median_household_income=>{[2005, 2009]=>41382}},
+           {:name=>"ADAMS COUNTY 14",
+              :median_household_income=>{[2006, 2010]=>40740}}],
+        "ADAMS-ARAPAHOE 28J"=>
+         [
+           {:name=>"ADAMS-ARAPAHOE 28J",
+              :median_household_income=>{[2005, 2009]=>43893}},
+            {:name=>"ADAMS-ARAPAHOE 28J",
+               :median_household_income=>{[2006, 2010]=>44007}}],
+        "AGATE 300"=>
+          [
+            {:name=>"AGATE 300",
+               :median_household_income=>{[2005, 2009]=>64167}},
+             {:name=>"AGATE 300",
+                :median_household_income=>{[2006, 2010]=>64145}}]}
+      assert_equal data,
+      group_by_district_name(
+      './test/data/median_household_income.csv', "median_household_income")
+    end
+
+
 
 end
