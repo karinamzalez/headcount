3258c46e2633d9450720f9b60a21f07546be11cb
Karina Gonzalez <karinamzalez@gmail.com>
2016-04-27 23:51:31 -0600

    median_household income parser completed

--- test/parser_median_income_test.rb
+++ test/parser_median_income_test.rb
@@ -4,116 +4,57 @@ SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/parser_median_income'
+require_relative '../lib/simplify_parsers_module'
 require 'csv'
 
 class ParserMedianIncomeTest < Minitest::Test
   include ParserMedianIncome
+  include SimplifyParsers
 
-  def test_it_can_get_raw_data_from_csv_file
-    output=
-    [
-      {
-        :location=>"Colorado", :timeframe=>"2005-2009",
-        :dataformat=>"Currency", :data=>"56222"
-      },
-      {
-        :location=>"Colorado", :timeframe=>"2006-2010",
-        :dataformat=>"Currency", :data=>"56456"
-      },
-      {
-        :location=>"ACADEMY 20", :timeframe=>"2005-2009",
-        :dataformat=>"Currency", :data=>"85060"
-      },
-      {
-        :location=>"ACADEMY 20", :timeframe=>"2006-2010",
-        :dataformat=>"Currency", :data=>"85450"
-      },
-      {
-        :location=>"ADAMS COUNTY 14", :timeframe=>"2005-2009",
-        :dataformat=>"Currency", :data=>"41382"
-      },
-      {
-        :location=>"ADAMS COUNTY 14", :timeframe=>"2006-2010",
-        :dataformat=>"Currency", :data=>"40740"
-      },
-      {
-        :location=>"ADAMS-ARAPAHOE 28J", :timeframe=>"2005-2009",
-        :dataformat=>"Currency", :data=>"43893"
-      },
-      {
-        :location=>"ADAMS-ARAPAHOE 28J", :timeframe=>"2006-2010",
-        :dataformat=>"Currency", :data=>"44007"
-      },
-      {
-        :location=>"AGATE 300", :timeframe=>"2005-2009",
-        :dataformat=>"Currency", :data=>"64167"
-      },
-      {
-        :location=>"AGATE 300", :timeframe=>"2006-2010",
-        :dataformat=>"Currency", :data=>"64145"
-      }
-    ]
-    assert_equal output, get_raw_data('./test/data/median_household_income.csv')
-  end
-
-  def test_it_can_delete_data_format
+  def test_it_can_format_each_median_income_line_into_a_hash
     output =
     [
       {
-        :location=>"Colorado",
-        :timeframe=>"2005-2009",
-        :data=>"56222"
+        :name=>"Colorado",
+        :median_household_income=>{[2005, 2009]=>56222}
       },
       {
-        :location=>"Colorado",
-        :timeframe=>"2006-2010",
-        :data=>"56456"
+        :name=>"Colorado",
+        :median_household_income=>{[2006, 2010]=>56456}
       },
       {
-        :location=>"ACADEMY 20",
-        :timeframe=>"2005-2009",
-        :data=>"85060"
+        :name=>"ACADEMY 20",
+        :median_household_income=>{[2005, 2009]=>85060}
       },
       {
-        :location=>"ACADEMY 20",
-        :timeframe=>"2006-2010",
-        :data=>"85450"
+        :name=>"ACADEMY 20",
+        :median_household_income=>{[2006, 2010]=>85450}
       },
       {
-        :location=>"ADAMS COUNTY 14",
-        :timeframe=>"2005-2009",
-        :data=>"41382"
+        :name=>"ADAMS COUNTY 14",
+        :median_household_income=>{[2005, 2009]=>41382}
       },
       {
-        :location=>"ADAMS COUNTY 14",
-        :timeframe=>"2006-2010",
-        :data=>"40740"
+        :name=>"ADAMS COUNTY 14",
+        :median_household_income=>{[2006, 2010]=>40740}
       },
       {
-        :location=>"ADAMS-ARAPAHOE 28J",
-        :timeframe=>"2005-2009",
-        :data=>"43893"
+        :name=>"ADAMS-ARAPAHOE 28J",
+        :median_household_income=>{[2005, 2009]=>43893}
       },
       {
-        :location=>"ADAMS-ARAPAHOE 28J",
-        :timeframe=>"2006-2010",
-        :data=>"44007"
+        :name=>"ADAMS-ARAPAHOE 28J",
+        :median_household_income=>{[2006, 2010]=>44007}
       },
       {
-        :location=>"AGATE 300",
-        :timeframe=>"2005-2009",
-        :data=>"64167"
+        :name=>"AGATE 300",
+        :median_household_income=>{[2005, 2009]=>64167}
       },
       {
-        :location=>"AGATE 300",
-        :timeframe=>"2006-2010",
-        :data=>"64145"
+        :name=>"AGATE 300",
+        :median_household_income=>{[2006, 2010]=>64145}
       }
     ]
-    assert_equal output, delete_dataformat('./test/data/median_household_income.csv')
-  end
-
-  def method_name
-
+    assert_equal output, format_median_income_hash_per_line('./test/data/median_household_income.csv', "median_household_income" )
   end
 end
