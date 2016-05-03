4d16eb6fca604164dd692af1eec0eff89b48d3c0
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-05-02 18:15:15 -0600

    almost done with top_statewide_test methods for iteration 5; need to incorporate weighted subjects and refactor

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -37,119 +37,119 @@ class HeadcountAnalystTest < Minitest::Test
     @ha = HeadcountAnalyst.new(@dr)
   end
 
-  # def test_it_can_find_average_enrollment_for_a_district
-  #   average_enrollment = @ha.average_enrollment_kindergarten("ACADEMY 20")
-  #   assert_equal 0.337, average_enrollment
-  # end
-  #
-  # def test_it_can_find_kindergarten_participation_rate_variation
-  #   assert_equal 1.002, @ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'COLORADO')
-  #   assert_equal 1.124, @ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'ADAMS COUNTY 14')
-  # end
-  #
-  # def test_it_can_find_variation_trends
-  #   hash = {2007 => 0.992, 2006 => 1.050, 2005 => 0.960}
-  #   assert_equal hash, @ha.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO')
-  # end
-  #
-  # def test_it_can_find_variation_between_kindergarten_and_high_school_graduation
-  #   assert_equal 0.819, @ha.kindergarten_participation_against_high_school_graduation("ACADEMY 20")
-  # end
-  #
-  # def test_it_can_determine_if_kindergarten_correlates_with_high_school_graduation
-  #   assert_equal true, @ha.kindergarten_participation_correlates_with_high_school_graduation(for: "ACADEMY 20")
-  # end
-  #
-  # def test_it_can_determine_if_kindergarten_correlates_with_high_school_graduation_statewide
-  #   assert_equal true, @ha.kindergarten_participation_correlates_with_high_school_graduation(for: "STATEWIDE")
-  # end
-  #
-  # def test_it_can_calculate_correlation_across_a_subset_of_districts
-  #   assert_equal true, @ha.kindergarten_participation_correlates_with_high_school_graduation(across: ['ACADEMY 20','ADAMS COUNTY 14'])
-  # end
-  #
-  # def test_it_can_get_the_grade
-  #   assert_equal 3, @ha.get_grade(grade: 3)
-  # end
-  #
-  # def test_you_must_provide_a_grade
-  #   assert_raises(InsufficientInformationError) do
-  #     @ha.get_grade(subject: :math)
-  #   end
-  # end
-  #
-  # def test_it_can_get_the_subject
-  #   assert_equal :math,
-  #   @ha.get_subject(subject: :math)
-  # end
-  #
-  # def test_it_can_truncate_percents
-  #   assert_equal 0.123, @ha.truncate_percents(0.12345)
-  # end
-  #
-  # def test_it_does_not_include_statewide_data
-  #   data =
-  #   [
-  #     ["COLORADO", 0.123],
-  #     ["ACADEMY 20", 0.345]
-  #   ]
-  #   refute @ha.ignore_statewide_data(data).flatten.include?("COLORADO")
-  # end
-  #
-  # def test_it_can_find_percentage_growth_for_one_district
-  #   @dr.load_data(@data)
-  #   d = @dr.districts[0]
-  #   assert_equal (-0.006),
-  #   @ha.find_percentage_growth_for_one_district({grade: 3, subject: :math}, d)
-  # end
-  #
-  #
-  # def test_it_can_find_a_single_leading_district_in_test_proficiency_growth
-  #   assert_equal ["ADAMS-ARAPAHOE 28J", -0.016],
-  #   @ha.top_statewide_test_year_over_year_growth(grade: 3, subject: :math)
-  # end
-  #
-  # def test_it_can_find_the_top_3_districts_in_growth
-  #   data =
-  #   [
-  #     ["ADAMS-ARAPAHOE 28J", -0.016],
-  #     ["ADAMS COUNTY 14", -0.02],
-  #     ["ACADEMY 20", -0.033]
-  #   ]
-  #   assert_equal data,
-  #   @ha.top_statewide_test_year_over_year_growth(grade: 3, top: 3, subject: :math)
-  # end
-  #
-  # def test_it_can_find_a_different_number_of_top_districts
-  #   data =
-  #   [
-  #     ["ADAMS-ARAPAHOE 28J", -0.016],
-  #     ["ADAMS COUNTY 14", -0.02],
-  #   ]
-  #   assert_equal data,
-  #   @ha.top_statewide_test_year_over_year_growth(grade: 3, top: 2, subject: :math)
-  # end
-  #
-  # def test_it_can_find_the_total_subject_proficiency_percent_for_first_year_of_one_district
-  #   d = @dr.find_by_name("ACADEMY 20")
-  #   assert_equal 0.798 ,
-  #   @ha.get_total_subject_percent_for_first_year({grade: 3}, d)
-  # end
-  #
-  # def test_it_can_find_the_total_subject_proficiency_percent_for_last_year_of_one_district
-  #   d = @dr.find_by_name("ACADEMY 20")
-  #   assert_equal 0.797 ,
-  #   @ha.get_total_subject_percent_for_last_year({grade: 3}, d)
-  # end
-  #
-  # def test_it_can_find_percentage_growth_across_all_subjects_for_one_district
-  #   d = @dr.find_by_name("ACADEMY 20")
-  #   assert_equal (-0.001) ,
-  #   @ha.find_percentage_growth_across_all_subjects({grade: 3}, d)
-  # end
+  def test_it_can_find_average_enrollment_for_a_district
+    average_enrollment = @ha.average_enrollment_kindergarten("ACADEMY 20")
+    assert_equal 0.337, average_enrollment
+  end
+
+  def test_it_can_find_kindergarten_participation_rate_variation
+    assert_equal 1.002, @ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'COLORADO')
+    assert_equal 1.124, @ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'ADAMS COUNTY 14')
+  end
+
+  def test_it_can_find_variation_trends
+    hash = {2007 => 0.992, 2006 => 1.050, 2005 => 0.960}
+    assert_equal hash, @ha.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO')
+  end
+
+  def test_it_can_find_variation_between_kindergarten_and_high_school_graduation
+    assert_equal 0.819, @ha.kindergarten_participation_against_high_school_graduation("ACADEMY 20")
+  end
+
+  def test_it_can_determine_if_kindergarten_correlates_with_high_school_graduation
+    assert_equal true, @ha.kindergarten_participation_correlates_with_high_school_graduation(for: "ACADEMY 20")
+  end
+
+  def test_it_can_determine_if_kindergarten_correlates_with_high_school_graduation_statewide
+    assert_equal true, @ha.kindergarten_participation_correlates_with_high_school_graduation(for: "STATEWIDE")
+  end
+
+  def test_it_can_calculate_correlation_across_a_subset_of_districts
+    assert_equal true, @ha.kindergarten_participation_correlates_with_high_school_graduation(across: ['ACADEMY 20','ADAMS COUNTY 14'])
+  end
+
+  def test_it_can_get_the_grade
+    assert_equal 3, @ha.get_grade(grade: 3)
+  end
+
+  def test_you_must_provide_a_grade
+    assert_raises(InsufficientInformationError) do
+      @ha.get_grade(subject: :math)
+    end
+  end
+
+  def test_it_can_get_the_subject
+    assert_equal :math,
+    @ha.get_subject(subject: :math)
+  end
+
+  def test_it_can_truncate_percents
+    assert_equal 0.123, @ha.truncate_percents(0.12345)
+  end
+
+  def test_it_does_not_include_statewide_data
+    data =
+    [
+      ["COLORADO", 0.123],
+      ["ACADEMY 20", 0.345]
+    ]
+    refute @ha.ignore_statewide_data(data).flatten.include?("COLORADO")
+  end
+
+  def test_it_can_find_percentage_growth_for_one_district
+    @dr.load_data(@data)
+    d = @dr.districts[0]
+    assert_equal (-0.006),
+    @ha.find_percentage_growth_for_one_district({grade: 3, subject: :math}, d)
+  end
+
+
+  def test_it_can_find_a_single_leading_district_in_test_proficiency_growth
+    assert_equal ["ADAMS-ARAPAHOE 28J", -0.016],
+    @ha.top_statewide_test_year_over_year_growth(grade: 3, subject: :math)
+  end
+
+  def test_it_can_find_the_top_3_districts_in_growth
+    data =
+    [
+      ["ADAMS-ARAPAHOE 28J", -0.016],
+      ["ADAMS COUNTY 14", -0.02],
+      ["ACADEMY 20", -0.033]
+    ]
+    assert_equal data,
+    @ha.top_statewide_test_year_over_year_growth(grade: 3, top: 3, subject: :math)
+  end
+
+  def test_it_can_find_a_different_number_of_top_districts
+    data =
+    [
+      ["ADAMS-ARAPAHOE 28J", -0.016],
+      ["ADAMS COUNTY 14", -0.02],
+    ]
+    assert_equal data,
+    @ha.top_statewide_test_year_over_year_growth(grade: 3, top: 2, subject: :math)
+  end
+
+  def test_it_can_find_the_total_subject_proficiency_percent_for_first_year_of_one_district
+    d = @dr.find_by_name("ACADEMY 20")
+    assert_equal 0.798 ,
+    @ha.get_total_subject_percent_for_first_year({grade: 3}, d)
+  end
+
+  def test_it_can_find_the_total_subject_proficiency_percent_for_last_year_of_one_district
+    d = @dr.find_by_name("ACADEMY 20")
+    assert_equal 0.797 ,
+    @ha.get_total_subject_percent_for_last_year({grade: 3}, d)
+  end
+
+  def test_it_can_find_percentage_growth_across_all_subjects_for_one_district
+    d = @dr.find_by_name("ACADEMY 20")
+    assert_equal (-0.001) ,
+    @ha.find_percentage_growth_across_all_subjects({grade: 3}, d)
+  end
 
   def test_it_can_find_the_top_district_across_all_subjects
-    assert_equal ["Something", 0.123],
+    assert_equal ["ADAMS-ARAPAHOE 28J", 0.006],
     @ha.top_statewide_test_year_over_year_growth(grade: 3)
   end
 
